a6085d9f3207c9d98d7ed336c308a7d42ee7af90
Tamsir bah <Bahtamsir91@gmail.com>
2024-12-05 15:06:10 +0100

    update v2

--- project/lib/budget-utils.ts
+++ project/lib/budget-utils.ts
@@ -1,3 +1,4 @@
 export function formatAmount(amount: number): string {
-  return `${amount.toFixed(1)} Mds €`
+  const formattedAmount = (amount / 1_000_000).toFixed(1); // Divide by 1 million for proper formatting
+  return `${formattedAmount} Mds €`;
 }
